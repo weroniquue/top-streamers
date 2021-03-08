ed0e43cb332f6c7668e47d7d2f0cdb2e795c2130
weroniquue <weronica97@gmail.com>
2021-03-08 18:22:50 +0100

    cross origin

--- top-streamers-backend/src/main/kotlin/com/put/topstreamers/api/StreamersController.kt
+++ top-streamers-backend/src/main/kotlin/com/put/topstreamers/api/StreamersController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("streamers")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = ["*"])
 class StreamersController(
     private val streamerService: StreamerService
 ) {
